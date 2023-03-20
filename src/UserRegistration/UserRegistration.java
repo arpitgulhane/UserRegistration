d8521ec401e6131cc95007ddf27906784d9c7abc
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-21 00:25:07 +0530

    Minimum char 3

---
+++ src/UserRegistration/UserRegistration.java
@@ -0,0 +1,16 @@
+package UserRegistration;
+
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+//        System.out.println(Pattern.matches(".s", "as"));//true (2nd char is s)
+
+//        boolean result = Pattern.matches("[6-9]{1}[0-9]{9}", "6888889999");
+//        boolean result = Pattern.matches("[a-zA-Z0-9]+[@][a-z]+[.][a-z]{2,}", "GOUTHAM123@microsoft.in");
+        boolean result = Pattern.matches("[A-Za-z]{1,3}", "GOp");
+        System.out.println("Given input is RIGHT ? \n"+result);
+    }
+
+}
