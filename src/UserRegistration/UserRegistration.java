632b4aa68a7c9ac75e979d01a7d078c3b577f116
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-21 10:42:25 +0530

    uc 2 Last name is 3 char

--- src/UserRegistration/UserRegistration.java
+++ src/UserRegistration/UserRegistration.java
@@ -10,7 +10,9 @@ public class UserRegistration {
 //        boolean result = Pattern.matches("[6-9]{1}[0-9]{9}", "6888889999");
 //        boolean result = Pattern.matches("[a-zA-Z0-9]+[@][a-z]+[.][a-z]{2,}", "GOUTHAM123@microsoft.in");
         boolean result = Pattern.matches("[A-Za-z]{3,10}", "GOOD");
+        boolean LastName = Pattern.matches("[A-Za-z]{3,10}", "Gulhane");
         System.out.println("Given input is RIGHT ? \n"+result);
+        System.out.println("LastName is Valid ? \n"+LastName);
     }
 
 }
